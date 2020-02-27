8671b935a4100fe86651244d96a9ca187def6887
Vijayant Satapara <vijayant.satapara@stridelysolutions.com>
2020-02-27 12:52:28 +0530

    added dacpac file

--- dbo/Tables/TestTabel.sql
+++ dbo/Tables/TestTabel.sql
@@ -2,6 +2,7 @@
     [TestTabelId] INT        NOT NULL,
     [TestName]    NCHAR (10) NULL,
     [Column1] INT NULL, 
+    [Column2] NCHAR(10) NULL, 
     PRIMARY KEY CLUSTERED ([TestTabelId] ASC)
 );
 
