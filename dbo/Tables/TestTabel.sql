c28f7f8befd17ff1e2f2edf2ab015ddd11c83481
Vijayant Satapara <vijayant.satapara@stridelysolutions.com>
2020-02-27 16:21:44 +0530

    clear cache

--- dbo/Tables/TestTabel.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TestTabel] (
-    [TestTabelId] INT        NOT NULL,
-    [TestName]    NCHAR (10) NULL,
-    [Column1] INT NULL, 
-    [NewColumn1] NCHAR(10) NULL, 
-    [NewColumn2] INT NULL, 
-    PRIMARY KEY CLUSTERED ([TestTabelId] ASC)
-);
-
