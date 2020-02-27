ef5bb09cb68620112a72a1bb2df197edf9f61e89
Vijayant Satapara <vijayant.satapara@stridelysolutions.com>
2020-02-27 15:18:28 +0530

    Alter and Add column

--- dbo/Tables/TestTabel.sql
+++ dbo/Tables/TestTabel.sql
@@ -2,7 +2,7 @@
     [TestTabelId] INT        NOT NULL,
     [TestName]    NCHAR (10) NULL,
     [Column1] INT NULL, 
-    [Column2] NCHAR(10) NULL, 
+    [NewColumn1] NCHAR(10) NULL, 
     PRIMARY KEY CLUSTERED ([TestTabelId] ASC)
 );
 
