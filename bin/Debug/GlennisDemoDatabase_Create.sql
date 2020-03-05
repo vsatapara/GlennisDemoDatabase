2986d2c0778c2d7145e1c881fb096647b971957c
Vijayant Satapara <vijayant.satapara@stridelysolutions.com>
2020-03-05 17:05:20 +0530

    Alter Table

--- bin/Debug/GlennisDemoDatabase_Create.sql
+++ bin/Debug/GlennisDemoDatabase_Create.sql
@@ -110,7 +110,8 @@ CREATE TABLE [dbo].[Table4] (
     [Id]     INT            NOT NULL,
     [Field1] VARBINARY (50) NULL,
     [Field2] NCHAR (10)     NULL,
-    [Field3] INT            NULL,
+    [Field4] NVARCHAR (50)  NULL,
+    [Field5] BIT            NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
--- dbo/Tables/Table4.sql
+++ dbo/Tables/Table4.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Field1] VARBINARY(50) NULL, 
     [Field2] NCHAR(10) NULL, 
-    [Field3] INT NULL
+    [Field4] NVARCHAR(50) NULL, 
+    [Field5] BIT NULL
 )
